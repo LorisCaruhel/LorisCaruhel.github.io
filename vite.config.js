719f2e362a412e2f95e431f1764839a01ac4d392
FlexKiWi1 <loriscaruhel3@gmail.com>
2024-04-10 19:14:25 +0200

    Suppression de plusiseurs node modules

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig} from 'vite'
-import react from '@vite.js/plugin-react'
-
-// https://vite.js.dev/config/
-export default defineConfig({
-    base: "/LorisCaruhel.github.io/",
-    plugins: [react()],
-})
